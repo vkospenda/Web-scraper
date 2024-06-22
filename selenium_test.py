34f6fc68c102dbfe196303af1c5dbc6f440b2cda
vkospenda <kospendavitan@gmail.com>
2024-06-22 08:53:41 +0200

    Dodal customer_request.txt, ki vsebuje želje strank. Program je zaustavljen na 2. strani, ker je očitno tretiran kot DDOS.

--- selenium_test.py
+++ selenium_test.py
@@ -1,3 +1,5 @@
+import time
+import random
 import pandas as pd
 import openpyxl
 from selenium import webdriver
@@ -6,10 +8,39 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# TODO: kako ne bit blokiran na 2. strani
+
 file = "cene.txt"
 
-driver = webdriver.Firefox()
+options = webdriver.FirefoxOptions()
+options.add_argument("--headless")
+options.add_argument("--disable-blink-features=AutomationControlled")
+
+driver = webdriver.Firefox(options=options)
 driver.get("https://www.nepremicnine.net/oglasi-prodaja/juzna-primorska/stanovanje/")
+
+def get_data():
+    cene = driver.find_elements(By.CSS_SELECTOR, "div.property-details h6")
+    for c in cene:
+        ls_cene.append(c.text)
+        
+    # Najdem mesto
+    mesta = driver.find_elements(By.CSS_SELECTOR, "a.url-title-d h2") 
+    for m in mesta:
+        ls_mesta.append(m.text)
+
+
+    # Locate all <ul> elements with the itemprop attribute 'disambiguatingDescription' - v teh elementih se nahaja kvadratura, leto ...
+    ul_elements = driver.find_elements(By.CSS_SELECTOR, 'ul[itemprop="disambiguatingDescription"]')
+
+    # Iterate through each <ul> element to find the text in the <li> element
+    for ul_element in ul_elements:
+        # Najdem kvadraturo in leto glede na pozicijo v <ul> elementu
+        kvadratura = ul_element.find_elements(By.TAG_NAME, 'li')[0]
+        leto = ul_element.find_elements(By.TAG_NAME, 'li')[1]
+
+        ls_kvadrature.append(kvadratura.text)
+        ls_leto.append(leto.text)
 
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "CybotCookiebotDialogBodyButtonAccept")))
 
@@ -36,29 +67,42 @@ ls_leto = []
 
 # Note to self: ko iščeš po css selectorju moreš "extractat" besedilo tako da daš <spremenljivka>.text
 # ker iščeš tag "a" s classom "url-title-d" daš piko, če pa bi iskal tag z ID bi dal "#"
+num_pages = int((driver.find_element(By.CSS_SELECTOR, "ul[data-pages]")).get_attribute("data-pages"))
+curr_page = 1
+prisotnost = True
 
-# Najdem cene
-cene = driver.find_elements(By.CSS_SELECTOR, "div.property-details h6")
-for c in cene:
-    ls_cene.append(c.text)
-    
-# Najdem mesto
-mesta = driver.find_elements(By.CSS_SELECTOR, "a.url-title-d h2") 
-for m in mesta:
-    ls_mesta.append(m.text)
+while(num_pages >= curr_page):
+    print(f"Procesiram stran {curr_page}")
+    get_data()
+    # # Najdem cene
+    # cene = driver.find_elements(By.CSS_SELECTOR, "div.property-details h6")
+    # for c in cene:
+    #     ls_cene.append(c.text)
+        
+    # # Najdem mesto
+    # mesta = driver.find_elements(By.CSS_SELECTOR, "a.url-title-d h2") 
+    # for m in mesta:
+    #     ls_mesta.append(m.text)
 
 
-# Locate all <ul> elements with the itemprop attribute 'disambiguatingDescription' - v teh elementih se nahaja kvadratura, leto ...
-ul_elements = driver.find_elements(By.CSS_SELECTOR, 'ul[itemprop="disambiguatingDescription"]')
+    # # Locate all <ul> elements with the itemprop attribute 'disambiguatingDescription' - v teh elementih se nahaja kvadratura, leto ...
+    # ul_elements = driver.find_elements(By.CSS_SELECTOR, 'ul[itemprop="disambiguatingDescription"]')
 
-# Iterate through each <ul> element to find the text in the <li> element
-for ul_element in ul_elements:
-    # Najdem kvadraturo in leto glede na pozicijo v <ul> elementu
-    kvadratura = ul_element.find_elements(By.TAG_NAME, 'li')[0]
-    leto = ul_element.find_elements(By.TAG_NAME, 'li')[1]
+    # # Iterate through each <ul> element to find the text in the <li> element
+    # for ul_element in ul_elements:
+    #     # Najdem kvadraturo in leto glede na pozicijo v <ul> elementu
+    #     kvadratura = ul_element.find_elements(By.TAG_NAME, 'li')[0]
+    #     leto = ul_element.find_elements(By.TAG_NAME, 'li')[1]
 
-    ls_kvadrature.append(kvadratura.text)
-    ls_leto.append(leto.text)
+    #     ls_kvadrature.append(kvadratura.text)
+    #     ls_leto.append(leto.text)
+
+        
+    curr_page += 1
+    next_btn = driver.find_element(By.CSS_SELECTOR, "a.next")
+    next_btn.click()
+    time.sleep(random.uniform(2, 5))
+        
 
 # ustvarim dataFrame
 df = pd.DataFrame(list(zip(ls_mesta, ls_cene, ls_kvadrature, ls_leto)))
@@ -67,3 +111,4 @@ print(df)
 
 driver.quit()
 print("Program je zaključil")
+
